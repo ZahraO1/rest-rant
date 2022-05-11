f0b48d91de458ecf3cbe742073a75239b50031e1
ZahraO1 <zahra.masroor6@gmail.com>
2022-05-11 19:00:08 -0400

    added an 404 image

--- views/error404.jsx
+++ views/error404.jsx
@@ -7,6 +7,7 @@ function error404(){
             <main>
                 <h1>404: PAGE NOT FOUND</h1>
                 <p>Oops, sorry, we can't find this page</p>
+                <img src='https://images.unsplash.com/photo-1573865526739-10659fec78a5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=415&q=80/40/40' alt='picture of cat'></img>
             </main>
         </Def>
     )
--- views/home.jsx
+++ views/home.jsx
@@ -8,6 +8,12 @@ function home(){
         <Def>
             <main>
                 <h1>HOME</h1>
+                <div>
+                    <img src="https://images.unsplash.com/photo-1490474504059-bf2db5ab2348?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80/40/40" alt="Chia Fruit Shake"></img>
+                    <div>
+                        Photo by <a href="AUTHOR_LINK">Brenda Godinez</a> on <a href="UNSPLASH_LINK">Unsplash</a>
+                    </div>
+                </div>
                 <a href="/places">
                     <button className="btn-primary">Places Page</button>
                 </a>
