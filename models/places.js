020967081f249216e6f055a4d5b7032e9435eae0
ZahraO1 <zahra.masroor6@gmail.com>
2022-05-25 06:32:07 -0400

    Navbar with style

--- models/places.js
+++ models/places.js
@@ -3,11 +3,13 @@ module.exports = [{
     city: 'Seattle',
     state: 'WA',
     cuisines: 'Thai, Pan-Asian',
-    pic: '/images/h-thai-ml-tables.jpg'
+    pic: '/images/h-thai-ml-tables.jpg',
+    pic:'https://images.unsplash.com/photo-1554679665-f5537f187268?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387'
 }, {
     name: 'Coding Cat Cafe',
     city: 'Phoenix',
     state: 'AZ',
     cuisines: 'Coffee, Bakery',
-    pic: '/images/coffee-cat.jpg'
+    pic: '/images/coffee-cat.jpg',
+    pic: 'https://cdn.pixabay.com/photo/2016/10/03/07/17/coffee-1711012_1280.jpg'
 }]
